3b4cb60b8b4245a7a065734e1fe8a7a2b2998c95
manishapansare11 <113811856+manishapansare11@users.noreply.github.com>
2022-09-18 07:57:50 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angularjs-d3-bar-chart-roq8dk
+
+[Edit on StackBlitz ⚡️](https://jake.stackblitz.com/edit/angularjs-d3-bar-chart-roq8dk)
---
+++ app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent implements OnInit  {
+  name = 'World';
+  chartData = [];
+
+  ngOnInit() {
+    for (let i = 0; i < (8 + Math.floor(Math.random() * 10)); i++) {
+      this.chartData.push([
+        `Index ${i}`,
+        Math.floor(Math.random() * 100)
+      ]);
+    }
+  }
+}
